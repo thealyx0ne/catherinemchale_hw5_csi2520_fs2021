3b19031e973a904bffa8a645363d852a80f88d11
Catt McHale <91563580+thealyx0ne@users.noreply.github.com>
2021-12-04 20:40:58 -0500

    Add files via upload

--- public/js/trivia.js
+++ public/js/trivia.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const ejs = require("ejs");
+const express = require('express');
+const ejs = require('ejs');
 
 //create express app
-const app = new express();
+const app = new express();''
 
 //initialize bodt parser middleware -- parses data sent in by users
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 //initialize ejs 
-app.set("view engine", "ejs");
-app.use("/public", express.static(_dirname + "/public"));
+app.set('view engine', 'ejs');
+app.use('/public', express.static(_dirname + '/public'));
 
 //server logic
 
@@ -19,76 +19,40 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get("/resume", (req, res) => {
-    res.render("./html/resume");
+app.get('/resume', (req, res) => {
+    res.render('./html/resume');
 });
 
-app.get("/funfacts", (req, res) => {
-    res.render("./html/funfacts");
+app.get('/funfacts', (req, res) => {
+    res.render('./html/funfacts');
 });
 
-app.get("trivia", (req, res) => {
-    res.render("./html/trivia");
+app.get('/trivia', (req, res) => {
+    let initialAnswers = 0;
+    res.render('./html/trivia.ejs', {
+        Answer1: initialAnswers,
+        Answer2: initialAnswers,
+        Answer3: initialAnswers,
+        Answer4: initialAnswers,
+        Answer5: initialAnswers
+    });
 });
 
-//question 1
-app.post('/url', (req, res) => {
-    let score = 0;
-    let data1 = req.body.answer1;
-    let answer1 = 'C';
-    if (data == answer1) {
-        score++;
-        res.send(`Correct!`);
-    }
-    else{
-        res.send(`Incorrect!`);
-    }
-
-//question 2
-    let data2 = req.body.answer2;
-    let answer2 = 'A';
-    if (data == answer2) {
-        score++;
-        res.send(`Correct!`);
-    }
-    else{
-        res.send(`Incorrect!`);
-    }
-
-//question 3
-    let data3 = req.body.answer3;
-    let answer3 = 'D';
-    if (data == answer3) {
-        score++;
-        res.send(`Correct!`);
-    }
-    else{
-        res.send(`Incorrect!`);
-    }
-
-//question 4
-    let data4 = req.body.answer4;
-    let answer4 = 'B';
-    if (data == answer4) {
-        score++;
-        res.send(`Correct!`);
-    }
-    else{
-        res.send(`Incorrect!`);
-    }
-
-//question 5
-    let data5 = req.body.answer5;
-    let answer5 = 'C';
-    if (data == answer5) {
-        score++;
-        res.send(`Correct!`);
-    }
-    else{
-        res.send(`Incorrect!`);
-    }
+app.post('/trivia', (req, res) => {
+    let newAnswer1 = req.body.userAnswer1;
+    let newAnswer2 = req.body.userAnswer2;
+    let newAnswer3 = req.body.userAnswer3;
+    let newAnswer4 = req.body.userAnswer4;
+    let newAnswer5 = req.body.userAnswer5;
+    req.render('./html/trivia', {
+        Answer1: newAnswer1,
+        Answer2: newAnswer2,
+        Answer3: newAnswer3,
+        Answer4: newAnswer4,
+        Answer5: newAnswer5,
+    });
 });
 
-//server
+//server port set up
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
